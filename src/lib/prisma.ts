78abaf9b8d1a584cad1a0b4f3ff1b161aef26bdd
Luqman <luqman@example.com>
2026-09-18 00:19:26 +0700

    fix: hardcode DB url to bypass Vercel env issue

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,9 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient();
+  new PrismaClient({
+    datasourceUrl: "postgresql://postgres.lzynugpistiyhxqkgwmk:Kuman123!%4021@aws-0-ap-southeast-2.pooler.supabase.com:6543/postgres?pgbouncer=true"
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
